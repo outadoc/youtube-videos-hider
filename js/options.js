e68e87efacc0756930958209f6eccb07eeb24009
outadoc <outadoc@gmail.com>
2012-11-03 00:10:32 +0100

    Fixed those fields

--- js/options.js
+++ js/options.js
@@ -9,6 +9,7 @@ $('input[name="save"]').click(function() {
 chrome.storage.sync.get('filters', function(item) {
 	console.log(item.filters);
 	for(var i = 0; i < item.filters.length; i++) {
-		$('#filters').append('<input type="text" class="author" value="' + item.filters[i].author + '"></input><input type="text" class="match" value="' + item.filters[i].match + '"<br />');
+		$('#filters').append('<input type="text" class="author" value="' + item.filters[i].author + '"></input> \
+						   	  <input type="text" class="match" value="' + item.filters[i].match + '"></input><br />');
 	}
 });
