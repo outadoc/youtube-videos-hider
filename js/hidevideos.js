e8c0967803c60ef5c4b00f0ae6df44566b625cbe
outadoc <outadoc@gmail.com>
2012-11-03 01:14:17 +0100

    Added more null checks

--- js/hidevideos.js
+++ js/hidevideos.js
@@ -1,15 +1,17 @@
 chrome.storage.sync.get('hiddenVideos', function(item) {	
-	var videos = $('.feed-page ul li');
-	var hidden_videos = (item.hiddenVideos).split('\n');
+	if(item.hiddenVideos != null) {
+		var videos = $('.feed-page ul li');
+		var hidden_videos = (item.hiddenVideos).split('\n');
 		
-	$.each(videos, function(i) {
-		var title = $(videos[i]).find('a.title').text();
-		
-		$.each(hidden_videos, function(j) {
-			if((title.toLowerCase()).indexOf((hidden_videos[j]).toLowerCase()) != -1) {
-				console.log('removing ' + title + '(because of ' + hidden_videos[j] + ')');
-				$(videos[i]).remove();
-			}
+		$.each(videos, function(i) {
+			var title = $(videos[i]).find('a.title').text();
+			
+			$.each(hidden_videos, function(j) {
+				if((title.toLowerCase()).indexOf((hidden_videos[j]).toLowerCase()) != -1) {
+					console.log('removing ' + title + '(because of ' + hidden_videos[j] + ')');
+					$(videos[i]).remove();
+				}
+			});
 		});
-	});
+	}
 });
--- js/options.js
+++ js/options.js
@@ -7,28 +7,29 @@ $('input[name="save"]').click(function() {
 });
 
 chrome.storage.sync.get('filters', function(item) {
-	console.log(item.filters);
-	var content = '<table><tr><th>Author</th><th>Title match</th></tr><tr>';
-	
-	for(var i = 0; i < item.filters.length; i++) {
-		if(item.filters[i].author == null) {
-			item.filters[i].author = '';
+	if(item.filters != null) {
+		var content = '<table><tr><th>Author</th><th>Title match</th></tr><tr>';
+		
+		for(var i = 0; i < item.filters.length; i++) {
+			if(item.filters[i].author == null) {
+				item.filters[i].author = '';
+			}
+			
+			if(item.filters[i].match == null) {
+				item.filters[i].match = '';
+			}
+			
+			content += '<tr><td><input type="text" class="author" value="' + item.filters[i].author + '"></input></td>';
+			content += '<td><input type="text" class="match" value="' + item.filters[i].match + '"></input></td>';
+			content += '<td><button class="remove">-</button>';
+			
+			if(i == item.filters.length - 1) {
+				content += '<button class="add">+</button>';
+			
+			}
+			content += '</td></tr>';
 		}
 		
-		if(item.filters[i].match == null) {
-			item.filters[i].match = '';
-		}
-		
-		content += '<tr><td><input type="text" class="author" value="' + item.filters[i].author + '"></input></td>';
-		content += '<td><input type="text" class="match" value="' + item.filters[i].match + '"></input></td>';
-		content += '<td><button class="remove">-</button>';
-		
-		if(i == item.filters.length - 1) {
-			content += '<button class="add">+</button>';
-		
-		}
-		content += '</td></tr>';
+		$('#filters').append(content + '</table>');
 	}
-	
-	$('#filters').append(content + '</table>');
 });
